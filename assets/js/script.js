9e1c32d690044772c106da79f6881b4084d169c5
cardinal <caren@merzhaus.org>
2022-05-08 11:47:24 -0400

    rough structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
+//vars
+
+var timerEl = document.querySelector("#timer");
+var main El = document.querySelector()
+
+var questionEl = document.querySelector(".question")
+var questionArr = [question1, question2, question3]
+
+var funcName = function() {
+
+    for (var i = 0; i < questionArr.length; i++) {
+        questionEl = questionObj.questionArr[i]
+    }
+}
+
+
+
+// //different way to move through questions without for loop?
+
+// questionArr[quetionIndex]
+
+// var questionFunc = function(){
+//     var moveForward = function(){
+//         displayNext()
+//         questionIndex++
+//     }
+
+//     if (correct){
+//         moveForward
+//     }
+// }
+
+
+
+
 var questionObj = [{
         question1: "",
         answers: [],
@@ -7,3 +42,5 @@ var questionObj = [{
         question2: "this is the next question: ",
     },
 ];
+
+alkdsj;;:
