c7283b79a5d315f8111f17f79ffc7b6ba3d53b2d
cardinal <caren@merzhaus.org>
2022-05-08 20:10:01 -0400

    timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,54 @@
-//vars
+// var timerEl = document.querySelector("#countdown");
+var countdownEl = document.getElementById('start-quiz');
+var timerEl = document.getElementById('countdown');
 
-var timerEl = document.querySelector("#timer");
-var main El = document.querySelector()
+function countdown() {
+    var timeLeft = 100;
 
-var questionEl = document.querySelector(".question")
-var questionArr = [question1, question2, question3]
+    var timeInterval = setInterval(function() {
 
-var funcName = function() {
-
-    for (var i = 0; i < questionArr.length; i++) {
-        questionEl = questionObj.questionArr[i]
-    }
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
 }
 
+//timer starts with click on Start Quiz button
+countdownEl.addEventListener("click", countdown);
 
 
-// //different way to move through questions without for loop?
-
-// questionArr[quetionIndex]
-
-// var questionFunc = function(){
-//     var moveForward = function(){
-//         displayNext()
-//         questionIndex++
-//     }
-
-//     if (correct){
-//         moveForward
-//     }
-// }
-
-
-
-
-var questionObj = [{
-        question1: "",
-        answers: [],
-        correct: "",
+var codeQuiz = [{
+        ques: "What is the answer to question 1?",
+        choices: ["flower", "tree", "herb", "chicken"],
+        correctAnswer: "chicken"
     },
     {
-        question2: "this is the next question: ",
-    },
-];
+        ques: "What is the answer to question 2?",
+        choices: ["milk", "butter", "soymilk", "oatmilk"],
+        correctAnswer: "butter"
+    }
+]; //end of object array
 
-alkdsj;;:
+
+
+
+//NOTES FROM OFFICE HOURS
+
+
+//question displays with 1 right answer button and 3 wrong answer buttons
+//if right answer is chosen, move on to next question &
+//display "correct" underneath question
+//else, 10 seconds are subtracted from timer
+//display "wrong" underneath question
+//enter initals field and submit button
+//go back button goes to quiz welcome and restarts timer
+//clear high scores button
+
+//NOTE: will need 4 event listeners for each button
